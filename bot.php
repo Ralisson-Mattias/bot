647f25b2f8011f43e83be73753fc3c00b5eafd7a
Ralisson-Mattias <68711690+Ralisson-Mattias@users.noreply.github.com>
2020-10-10 22:16:56 -0300

    002

--- bot.php
+++ bot.php
@@ -24,30 +24,85 @@
 
 <body>
 
-    <div class="wrapper">
-        <div class="title">Chatbot</div>
-        <div class="form">
-            <div class="bot-inbox inbox">
-                <div class="icon">
-                    <i class="fas fa-user"></i>
-                </div>
-                <div class="msg-header">
-                    <p id="protocol-info">
-                    </p>
+    <div id="wrapper-button" class="wrapper-button">
+        <i class="fas fa-headset fa-2x"></i>
+    </div>
+
+    <div id="wrapper-box" class="wrapper-box-exit">
+        <div class="wrapper">
+            <div class="title">
+                <div id="sub-title" class="sub-title">
+                    Chatbot
+                    <i class="fas fa-arrow-down"></i>
                 </div>
             </div>
+            <div class="form">
+                <div class="bot-inbox inbox">
+                    <div class="icon">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="msg-header">
+                        <p id="protocol-info">
+                        </p>
+                    </div>
+                </div>
 
-        </div>
-        <div class="typing-field">
-            <div class="input-data">
-                <input id="data" type="text" placeholder="Digite algo aqui" required>
-                <button id="send-btn">Enviar</button>
+            </div>
+            <div class="typing-field">
+                <div class="input-data">
+                    <input id="data" type="text" placeholder="Digite algo aqui" required>
+                    <button id="send-btn">Enviar</button>
+                </div>
             </div>
         </div>
     </div>
 
     <script>
         $(document).ready(function() {
+
+
+
+            function getPosicaoElemento(elemID) {
+                var offsetTrail = document.getElementById(elemID);
+                var offsetLeft = 0;
+                var offsetTop = 0;
+                while (offsetTrail) {
+                    offsetLeft += offsetTrail.offsetLeft;
+                    offsetTop += offsetTrail.offsetTop;
+                    offsetTrail = offsetTrail.offsetParent;
+                }
+                if (navigator.userAgent.indexOf("Mac") != -1 &&
+                    typeof document.body.leftMargin != "undefined") {
+                    offsetLeft += document.body.leftMargin;
+                    offsetTop += document.body.topMargin;
+                }
+                return {
+                    left: offsetLeft,
+                    top: offsetTop
+                };
+            }
+
+            console.log("esquerda:" + getPosicaoElemento("wrapper-box").left)
+            console.log("topo:" + getPosicaoElemento("wrapper-box").top)
+
+
+            let down = $('#wrapper-box').hasClass('wrapper-down')
+            let up = $('#wrapper-box').hasClass('wrapper-box-enter')
+
+            $('#wrapper-button').on('click', () => {
+                $('#wrapper-box').addClass('wrapper-box-enter')
+
+                console.log("esquerda:" + getPosicaoElemento("wrapper-box").left)
+                console.log("topo:" + getPosicaoElemento("wrapper-box").top)
+            })
+            
+            $('#sub-title').on('click', () => {
+                $('#wrapper-box').addClass('wrapper-down')
+
+                console.log("esquerda:" + getPosicaoElemento("wrapper-box").left)
+                console.log("topo:" + getPosicaoElemento("wrapper-box").top)
+            })
+
 
             var aleatorio = Math.floor(Math.random() * 100000000)
             $('#protocol-info').html('Olá, como posso te ajudar?' + '<br>' + 'Seu número de protocolo é ' + aleatorio)
@@ -70,7 +125,6 @@
                     type: 'POST',
                     data: 'text=' + $value,
                     success: function(result) {
-
                         $replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>' + result + '</p></div></div>';
                         $(".form").append($replay);
                         $(".form").scrollTop($(".form")[0].scrollHeight);
